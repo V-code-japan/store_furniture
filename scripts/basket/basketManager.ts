55e174c9206f454b9adf1c5d814486e4cc5466b2
hghrktkr <t.hagihara@anem.co.jp>
2026-09-09 14:36:51 +0900

    edu:basket追加

---
+++ scripts/basket/basketManager.ts
@@ -0,0 +1,26 @@
+import { BlockComponentOnPlaceEvent, BlockPermutation, StartupEvent } from "@minecraft/server";
+import { sendSystemMessage } from "../util/util";
+
+export class BasketManager {
+  static registerComponent(startupEv: StartupEvent) {
+    startupEv.blockComponentRegistry.registerCustomComponent("edu:set_basket_state", {
+      onPlace: (ev) => this.onPlace(ev),
+    });
+  }
+
+  static onPlace(ev: BlockComponentOnPlaceEvent) {
+    // ランダムにedu:vegitable_typeを変更
+    const type = Math.floor(Math.random() * 3);
+    // 現在のdirectionを取得
+    const dir = ev.block.permutation.getState("minecraft:cardinal_direction");
+
+    if (dir === undefined) {
+      sendSystemMessage(`[BasketManager onPlace] ${ev.block.typeId}の"minecraft:cardinal_directionがundefinedです`);
+      throw new Error(`[BasketManager onPlace] ${ev.block.typeId}の"minecraft:cardinal_directionがundefinedです`);
+    }
+
+    const perm = ev.block.setPermutation(
+      BlockPermutation.resolve("edu:basket", { "edu:vegitable_type": type, "minecraft:cardinal_direction": dir })
+    );
+  }
+}
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,10 +1,12 @@
 import { StartupEvent, system } from "@minecraft/server";
 import { WalkInCoolerManager } from "./walkInCooler/walkInCoolerManager";
 import { sendSystemMessage } from "./util/util";
+import { BasketManager } from "./basket/basketManager";
 
 // カスタムコンポーネント登録
 system.beforeEvents.startup.subscribe((ev: StartupEvent) => {
   WalkInCoolerManager.registerComponent(ev);
+  BasketManager.registerComponent(ev);
 });
 
 system.afterEvents.scriptEventReceive.subscribe((ev) => {
